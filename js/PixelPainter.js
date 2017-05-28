94a8a3265c7e0298d32497df98d6dbda7bee33e5
Alexis <alexisko@hawaii.edu>
2017-05-27 16:07:22 -1000

    Canvas/Table clicking and changing colors finally working!!

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,27 +1,53 @@
-var ppDiv = document.createElement("div");
-ppDiv.setAttribute("id", "pp-canvas");
-document.getElementById("pixelPainter").appendChild(ppDiv);
+//
+function canvas() {
+  var ppDiv = document.createElement("div");
+  ppDiv.setAttribute("id", "pp-canvas");
+  document.getElementById("pixelPainter").appendChild(ppDiv);
 
-var ppTable = document.createElement("table");
-ppTable.setAttribute("id", "pp-table");
-document.getElementById("pp-canvas").appendChild(ppTable);
+  var ppTable = document.createElement("table");
+  ppTable.setAttribute("id", "pp-table");
+  document.getElementById("pp-canvas").appendChild(ppTable);
 
-var height = 5;
-var width = 5;
+  function createCanvas(height, width) {
+    var table = [];
+    for(var i = 0; i < height; i++) { //rows
+      table[i] = document.createElement("tr");
+      document.getElementById('pp-table').appendChild(table[i]);
+      table[i].id = 'Row' + i;
+      table[i] = [];
 
-var td = document.createElement("td");
-var tr = document.createElement("tr");
-
-var rowCol = [];
-for(var i = 0; i < height; i++) {
-  rowCol[i] = document.createElement("tr");
-  rowCol[i].id = 'Row' + i;
-  document.getElementById('pp-table').appendChild(rowCol[i]);
-  rowCol[i] = [];
-  for(var j = 0; j < width; j++) {
-    rowCol[i][j] = document.createElement("td");
-    document.getElementById('Row' + i).appendChild(rowCol[i][j]);
+      for(var j = 0; j < width; j++) { //cols
+        table[i][j] = document.createElement("td");
+        table[i][j].addEventListener('click', changeColor);
+        document.getElementById('Row' + i).appendChild(table[i][j]);
+      }
+    }
+    return table;
   }
+
+  // ppTable.addEventListener('click', function (event) {
+  //   event.currentTarget.style.backgroundColor = "yellow";
+  // });
+
+  function changeColor() {
+    event.currentTarget.style.backgroundColor = "yellow";
+  }
+
+  return {
+    createCanvas: createCanvas,
+    changeColor: changeColor
+  };
 }
 
-console.log("check rowcol", rowCol[1][0]);
+function colorPalette() {
+  var ppDiv = document.createElement("div");
+  ppDiv.setAttribute("id", "pp-colorPalette");
+  document.getElementById("pixelPainter").appendChild(ppDiv);
+
+  var ppTable = document.createElement("table");
+  ppTable.setAttribute("id", "pp-colorTable");
+  document.getElementById("pp-colorPalette").appendChild(ppTable);
+}
+
+var canvas = canvas();
+var table = canvas.createCanvas(5, 5);
