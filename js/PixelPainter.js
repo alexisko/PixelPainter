f1a2735422920ea4041c186148acebc0316cc580
Alexis <alexisko@hawaii.edu>
2017-05-27 18:46:16 -1000

    Starting css

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,53 +1,83 @@
-//
-function canvas() {
-  var ppDiv = document.createElement("div");
-  ppDiv.setAttribute("id", "pp-canvas");
-  document.getElementById("pixelPainter").appendChild(ppDiv);
+//global variable
+var curColor;
 
-  var ppTable = document.createElement("table");
-  ppTable.setAttribute("id", "pp-table");
-  document.getElementById("pp-canvas").appendChild(ppTable);
+//functions
+function canvas() {
+  var ppTable = document.createElement("div");
+  ppTable.setAttribute("id", "pp-grid");
+  document.getElementById("pixelPainter").appendChild(ppTable);
 
   function createCanvas(height, width) {
     var table = [];
     for(var i = 0; i < height; i++) { //rows
-      table[i] = document.createElement("tr");
-      document.getElementById('pp-table').appendChild(table[i]);
-      table[i].id = 'Row' + i;
+      table[i] = document.createElement("div");
+      document.getElementById('pp-grid').appendChild(table[i]);
+      table[i].className = 'divRowGrid';
+      table[i].id = 'gridRow' + i;
       table[i] = [];
 
       for(var j = 0; j < width; j++) { //cols
-        table[i][j] = document.createElement("td");
-        table[i][j].addEventListener('click', changeColor);
-        document.getElementById('Row' + i).appendChild(table[i][j]);
+        table[i][j] = document.createElement("div");
+        table[i][j].style.backgroundColor = 'white';
+        table[i][j].className = 'divCellGrid';
+        table[i][j].addEventListener('click', changeBackgroundColor);
+        document.getElementById('gridRow' + i).appendChild(table[i][j]);
       }
     }
-    return table;
   }
 
-  // ppTable.addEventListener('click', function (event) {
-  //   event.currentTarget.style.backgroundColor = "yellow";
-  // });
-
-  function changeColor() {
-    event.currentTarget.style.backgroundColor = "yellow";
+  function changeBackgroundColor() {
+    event.currentTarget.style.backgroundColor = curColor;
   }
 
   return {
     createCanvas: createCanvas,
-    changeColor: changeColor
+    changeBackgroundColor: changeBackgroundColor
   };
 }
 
 function colorPalette() {
-  var ppDiv = document.createElement("div");
-  ppDiv.setAttribute("id", "pp-colorPalette");
-  document.getElementById("pixelPainter").appendChild(ppDiv);
-
-  var ppTable = document.createElement("table");
+  var ppTable = document.createElement("div");
   ppTable.setAttribute("id", "pp-colorTable");
-  document.getElementById("pp-colorPalette").appendChild(ppTable);
+  document.getElementById("pixelPainter").appendChild(ppTable);
+
+  var table = [];
+  var height = 5;
+  var width = 5;
+  for(var i = 0; i < height; i++) { //rows
+    table[i] = document.createElement("div");
+    document.getElementById('pp-colorTable').appendChild(table[i]);
+    table[i].className = 'divRowColor';
+    table[i].id = 'colorRow' + i;
+    table[i] = [];
+
+    for(var j = 0; j < width; j++) { //cols
+      table[i][j] = document.createElement("div");
+      table[i][j].addEventListener('click', changeColor);
+      table[i][j].style.backgroundColor = getRandomColor();
+      table[i][j].className = 'divCellColor';
+      document.getElementById('colorRow' + i).appendChild(table[i][j]);
+    }
+  }
+
+  /*
+   * Random Color Generator function via:
+   * https://stackoverflow.com/questions/1484506/random-color-generator-in-javascript
+   */
+  function getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++ ) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+  }
+
+  function changeColor() {
+    curColor = event.currentTarget.style.backgroundColor;
+  }
 }
 
 var canvas = canvas();
-var table = canvas.createCanvas(5, 5);
+canvas.createCanvas(5, 5);
+var colorPalette = colorPalette();
