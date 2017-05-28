ad5a4c0875dc2e0b4eaf25aba69ec1d8f8390b52
Alexis <alexisko@hawaii.edu>
2017-05-27 14:57:00 -1000

    table works now! td/trs appened to doc!

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -9,17 +9,19 @@ document.getElementById("pp-canvas").appendChild(ppTable);
 var height = 5;
 var width = 5;
 
-var counter = 0;
+var td = document.createElement("td");
+var tr = document.createElement("tr");
 
 var rowCol = [];
 for(var i = 0; i < height; i++) {
+  rowCol[i] = document.createElement("tr");
+  rowCol[i].id = 'Row' + i;
+  document.getElementById('pp-table').appendChild(rowCol[i]);
   rowCol[i] = [];
   for(var j = 0; j < width; j++) {
-    rowCol[i][j] = '<td>' + 'a' + '</td>';
+    rowCol[i][j] = document.createElement("td");
+    document.getElementById('Row' + i).appendChild(rowCol[i][j]);
   }
-  rowCol[i] = '<tr>' + rowCol[i] + '</tr>';
 }
 
-var test = document.getElementById('pp-table').innerHTML = rowCol;
-
-rowCol[1][1] = '<td>' + 'bfjadk' + '</td>';
+console.log("check rowcol", rowCol[1][0]);
