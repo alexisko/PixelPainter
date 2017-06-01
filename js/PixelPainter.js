8f1f5124b492e7faaa924ba4cf57c6d7e260baa2
Alexis <alexisko@hawaii.edu>
2017-05-31 18:36:37 -1000

    All buttons working, now going back to working on css

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -4,14 +4,23 @@ var colorBox;
 
 //functions
 function canvas() {
+  var canvasDiv = document.createElement("div");
+  canvasDiv.setAttribute("id", "canvasDiv");
+  document.getElementById("pixelPainter").appendChild(canvasDiv);
+
+  var gridDiv = document.createElement("div");
+  gridDiv.setAttribute("id", "gridDiv");
+  document.getElementById("canvasDiv").appendChild(gridDiv);
+
   var grid = document.createElement("div");
   grid.setAttribute("id", "pp-grid");
-  document.getElementById("pixelPainter").appendChild(grid);
+  document.getElementById("gridDiv").appendChild(grid);
 
   var table = [];
-
+  var savedArray = [];
   //creates the grid for users to draw on
   function createCanvas(height, width) {
+
     for(var i = 0; i < height; i++) { //rows
       table[i] = document.createElement("div");
       document.getElementById('pp-grid').appendChild(table[i]);
@@ -23,6 +32,7 @@ function canvas() {
         table[i][j] = document.createElement("div");
         table[i][j].style.backgroundColor = 'white';
         table[i][j].className = 'divCellGrid';
+        table[i][j].addEventListener('mouseover', changeBackgroundColor2);
         table[i][j].addEventListener('click', changeBackgroundColor);
         document.getElementById('gridRow' + i).appendChild(table[i][j]);
       }
@@ -34,6 +44,12 @@ function canvas() {
     event.currentTarget.style.backgroundColor = curColor;
   }
 
+  function changeBackgroundColor2() {
+    if(event.buttons === 1) {
+      event.currentTarget.style.backgroundColor = curColor;
+    }
+  }
+
   //sets all cell's background color to white
   function clear() {
     for(var i = 0; i < table.length; i++) {
@@ -43,21 +59,46 @@ function canvas() {
     }
   }
 
+  function save(height, width) {
+    for(var i = 0; i < height; i++) {
+      savedArray[i] = document.createElement("div");
+      savedArray[i] = [];
+      for(var j = 0; j < width; j++) {
+        savedArray[i][j] = document.createElement("div");
+        savedArray[i][j].style.backgroundColor = table[i][j].style.backgroundColor;
+      }
+    }
+  }
+
+  function loadSave(height, width) {
+    for(var i = 0; i < height; i++) {
+      for(var j = 0; j < width; j++) {
+        table[i][j].style.backgroundColor = savedArray[i][j].style.backgroundColor;
+      }
+    }
+  }
+
   return {
     createCanvas: createCanvas,
     clear: clear,
-    changeBackgroundColor: changeBackgroundColor
+    changeBackgroundColor: changeBackgroundColor,
+    save: save,
+    loadSave: loadSave
   };
 }
 
 function colorPalette() {
+  colorDiv = document.createElement("div");
+  colorDiv.setAttribute("id", "colorDiv");
+  document.getElementById("pixelPainter").appendChild(colorDiv);
+
   colorBox = document.createElement("div");
   colorBox.setAttribute("id", "colorBox");
-  document.getElementById("colorPalette").appendChild(colorBox);
+  document.getElementById("colorDiv").appendChild(colorBox);
 
   var palette = document.createElement("div");
   palette.setAttribute("id", "pp-colorTable");
-  document.getElementById("colorPalette").appendChild(palette);
+  document.getElementById("colorDiv").appendChild(palette);
 
   //creates a color palette
   var table = [];
@@ -92,44 +133,116 @@ function colorPalette() {
         return color;
   }
 
+  function reRandomizeColors() {
+    for(var i = 0; i < height; i++) {
+      for(var j = 0; j < width; j++) {
+        table[i][j].style.backgroundColor = getRandomColor();
+      }
+    }
+  }
+
   //sets the current color to whatever the user clicked
   function changeColor() {
     curColor = event.currentTarget.style.backgroundColor;
     colorBox.style.backgroundColor = curColor;
   }
+
+  return {
+    reRandomizeColors: reRandomizeColors
+  };
 }
+
+function buttons() {
+  var buttonDiv = document.createElement("div");
+  buttonDiv.setAttribute("id", "buttonDiv");
+  document.getElementById("pixelPainter").appendChild(buttonDiv);
+
+  //clear button
+  var clearBtn = document.createElement("button");
+  clearBtn.innerHTML = "clear";
+  clearBtn.type = "button";
+  document.getElementById("buttonDiv").appendChild(clearBtn);
+
+  clearBtn.addEventListener('click', function(event) {
+    canvas.clear();
+  });
+
+  //erase button
+  var eraseBtn = document.createElement("button");
+  eraseBtn.innerHTML = "erase";
+  eraseBtn.type = "button";
+  document.getElementById("buttonDiv").appendChild(eraseBtn);
+
+  eraseBtn.addEventListener('click', function(event) {
+    curColor = 'white';
+    colorBox.style.backgroundColor = curColor;
+  });
+
+  //save button
+  var saveBtn = document.createElement("button");
+  saveBtn.innerHTML = "save";
+  saveBtn.type = "button";
+  document.getElementById("buttonDiv").appendChild(saveBtn);
+
+  saveBtn.addEventListener('click', function(event) {
+    canvas.save(20, 20);
+  });
+
+  //load button
+  var loadBtn = document.createElement("button");
+  loadBtn.innerHTML = "load";
+  loadBtn.type = "button";
+  document.getElementById("buttonDiv").appendChild(loadBtn);
+
+  loadBtn.addEventListener('click', function(event) {
+    canvas.loadSave(20, 20);
+  });
+}
+
+//
+// var sizeDiv = document.createElement("div");
+// sizeDiv.id = "sizeDiv";
+// document.getElementById("pixelPainter").appendChild(sizeDiv);
+
+// var plusSizeBtn = document.createElement("button");
+// plusSizeBtn.innerHTML = "+";
+// plusSizeBtn.id = "plusSizeBtn";
+// plusSizeBtn.type = "button";
+
+// var minusSizeBtn = document.createElement("button");
+// minusSizeBtn.innerHTML = "-";
+// minusSizeBtn.id = "minusSizeBtn";
+// minusSizeBtn.type = "button";
+
+// var height = document.createElement("div");
+// height.id = "height";
+// height.innerHTML = 20;
+
+// var times = document.createElement("div");
+// times.id = "times";
+// times.innerHTML = "x";
+
+// var width = document.createElement("div");
+// width.id = "width";
+// width.innerHTML = 20;
+
+// document.getElementById("sizeDiv").appendChild(plusSizeBtn);
+// document.getElementById("sizeDiv").appendChild(height);
+// document.getElementById("sizeDiv").appendChild(times);
+// document.getElementById("sizeDiv").appendChild(width);
+// document.getElementById("sizeDiv").appendChild(minusSizeBtn);
+
+// plusSizeBtn.addEventListener('click', function(event) {
+//   var widthInt = parseInt(width.innerHTML);
+//   var heightInt = parseInt(height.innerHTML);
+//     if(widthInt <= 20) {
+//       widthInt++;
+
+//     }
+//   });
 
 var canvas = canvas();
 canvas.createCanvas(20, 20);
 var colorPalette = colorPalette();
+var buttons = buttons();
 
-//clear button
-var clearBtn = document.createElement("button");
-clearBtn.innerHTML = "clear";
-clearBtn.type = "button";
-document.getElementById("buttons").appendChild(clearBtn);
-
-clearBtn.addEventListener('click', function(event) {
-  canvas.clear();
-});
-
-//erase button
-var eraseBtn = document.createElement("button");
-eraseBtn.innerHTML = "erase";
-eraseBtn.type = "button";
-document.getElementById("buttons").appendChild(eraseBtn);
-
-eraseBtn.addEventListener('click', function(event) {
-  curColor = 'white';
-  colorBox.style.backgroundColor = curColor;
-});
-
-//save button
-var saveBtn = document.createElement("button");
-saveBtn.innerHTML = "save";
-saveBtn.type = "button";
-document.getElementById("buttons").appendChild(saveBtn);
-
-saveBtn.addEventListener('click', function(event) {
-  curColor = 'white';
-});
